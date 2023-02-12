3e44f9e3781c1e358459376cf37f5185e8de0d82
nuriaruiz <nuria.ruiz@gmail.com>
2023-02-12 19:53:14 +0100

    example simple search

---
+++ src/main/java/com/nuria/htmxdemo/HtmxController.java
@@ -0,0 +1,54 @@
+package com.nuria.htmxdemo;
+
+import io.github.wimdeblauwe.hsbt.mvc.HtmxRequest;
+import io.github.wimdeblauwe.hsbt.mvc.HxRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/")
+public class HtmxController {
+
+    @PostMapping("/search")
+    @HxRequest
+    public String htmxRequest(HtmxRequest details, @RequestBody String searchTextUser, Model model) {
+        String searchText = searchTextUser.substring(searchTextUser.indexOf("=")+1);
+        List<String> countries = List.of("Afghanistan", "Albania", "Algeria", "Andorra", "Angola",
+                "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+                "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus",
+                "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina",
+                "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
+                "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic",
+                "Chad", "Chile", "China", "Colombia", "Comoros", "Congo",
+                "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus",
+                "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+                "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
+                "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France",
+                "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada",
+                "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras",
+                "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq",
+                "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
+                "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan",
+                "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
+                "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi",
+                "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
+                "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
+                "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique",
+                "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand",
+                "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia",
+                "Norway");
+        countries = countries.stream()
+                .filter(country -> country.toLowerCase().contains(searchText))
+                .collect(Collectors.toList());
+        model.addAttribute("countries", countries);
+        return "search-results";
+    }
+
+
+}
